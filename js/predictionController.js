c37dc01315fbb7987da0e67f1f0f9b04ea8ac91c
machinotori <yuto140816@gmail.com>
2024-02-11 13:26:31 +0900

    predictionController.jsの目標金額登録処理

--- js/predictionController.js
+++ js/predictionController.js
@@ -9,10 +9,11 @@ const vm = new Vue({
     mounted() {
       
     },
+  
     methods: {
-        //ユーザーログイン処理
+        //目標金額を更新する
         updateTargetAmount() {          
-            const url = "./application/api/userLogin.php";
+            const url = "./application/api/setTargetAmount.php";
             const data = new FormData();
             data.append('user_mail', this.user_mail);
             data.append('user_pass', this.user_pass);
@@ -20,17 +21,12 @@ const vm = new Vue({
             axios.post(url, data)
               .then(response => {
                 // レスポンスを処理するコード
-                if(typeof(response.data) == 'object'){
-                  console.log("成功時分岐");
-                  console.log(response.data);
-                  sessionStorage.setItem('id',response.data['user_id']);
-                  sessionStorage.setItem('name',response.data['user_name']);
-                  sessionStorage.setItem('targetAmount',response.data['targetAmount']);
-                  window.location.assign("./spend_prediction.html");
+                if(typeof(response.data) == 0){
+                  console.log("目標金額の登録完了!");
+                  window.location.assign("./spend_input.html");
                 }else if(response.data === 1){
-                  console.log("失敗時分岐");
+                  console.log("登録に失敗しました");
                   console.log(response.data);
-                  this.error = "メールアドレスまたはパスワードが間違っています。";
                 } else {
                   console.log("異常終了");
                 }
@@ -50,5 +46,75 @@ const vm = new Vue({
           const urlParams = new URLSearchParams(window.location.search);
           return urlParams.get(paramName);
         }
+    },
+
+    methods: {
+      //支出登録処理
+      set_amounts_data() {          
+          const url = "./application/api/amount_data_set.php";
+          const data = new FormData();
+          data.append('dow', this.dow);
+          data.append('Amount', this.totalNum);
+          data.append('user_id',this.user_id);
+          data.append('food_ex',this.food_ex);
+          data.append('trans_ex',this.trans_ex);
+          data.append('enterme_ex',this.enterme_ex);
+          data.append('others',this.others);
+          data.append('today',this.getDate_today(0));
+          
+          axios.post(url, data)
+            .then(response => {
+              // レスポンスを処理するコード
+              alert(response.data);
+              this.saving = this.exAmount - this.totalNum;
+            })
+            .catch(error => {
+              // エラーハンドリングのコード
+              console.error(error);
+            });
       },
+      get_examount(){
+          const url = "./application/api/examount_get.php";
+          const data = new FormData();
+          data.append('user_id',this.user_id);
+          data.append('dow', this.dow);
+
+          axios.post(url, data)
+            .then(response => {
+              // レスポンスを処理するコード
+              this.exAmount = response.data;
+            })
+            .catch(error => {
+              // エラーハンドリングのコード
+              console.error(error);
+            });
+      },
+      // 特定のGETパラメータが存在するかどうかを確認する関数
+      checkQueryParamExists(paramName) {
+        const searchParams = new URLSearchParams(window.location.search);
+        return searchParams.has(paramName);
+      },
+      //特定のGETパラメータを取得する関数
+      getQueryParam(paramName) {
+        const urlParams = new URLSearchParams(window.location.search);
+        return urlParams.get(paramName);
+      },
+      
+      //曜日を取得する関数
+      getDate_dow(){
+          var WeekChars = [ "日", "月", "火", "水", "木", "金", "土" ];
+          var dObj = new Date();
+          var wDay = dObj.getDay();
+
+          console.log(WeekChars[wDay]);
+
+          return WeekChars[wDay];
+      }
+    },
+    computed: {
+      totalNum(){
+          return this.food_ex + this.trans_ex + this.enterme_ex + this.others
+      }
+    },
+
 })
